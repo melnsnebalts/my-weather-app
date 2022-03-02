13f89879de73fd39f4d945428c8989049344571e
Elīza <eliza.kalnina37@gmail.com>
2022-03-03 00:27:47 +0200

    Removed console log coordinates JS

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ function showLocation(position) {
 
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(currentData);
-
-  console.log(lat);
-  console.log(lon);
 }
 function locationClick(event) {
   event.preventDefault();
@@ -107,7 +104,6 @@ forecast.forEach(function (forecastDay, index) {
   }
   
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "7217515dc130401eb9daec1124e1a28f";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
